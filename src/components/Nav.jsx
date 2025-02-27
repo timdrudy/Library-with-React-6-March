681c702efbe4c894744203bcacfe899754da4f57
Tim <timdrudy@gmail.com>
2025-02-27 13:54:44 -0600

    edited

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = ( ) => {
+    return (
+        <nav>
+            <div className="nav__container">
+                <a href="/">
+                    <img src="" alt="" className="logo" />
+                </a>
+                <ul className="nav__links">
+                    <li className="nav__list">
+                        <a href="/" className="nav__link"></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default Nav;
