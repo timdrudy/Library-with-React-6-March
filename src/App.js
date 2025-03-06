66cdee002f74a44b7fa09112ab6de3abeed422cf
Tim <timdrudy@gmail.com>
2025-03-06 11:29:39 -0600

    deleted project to re-initialize

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import Discounted from './components/Discounted';
+import Explore from './components/Explore';
+import Featured from './components/Featured';
+import Footer from './components/Footer';
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
 import Nav from './components/Nav';
 
 
@@ -5,6 +11,12 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBars, faShoppingCart, faTimes)
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt)
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
