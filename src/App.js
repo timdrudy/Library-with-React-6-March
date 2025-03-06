08412249b39ab157b4628f35a91c460f5ce61228
Tim <timdrudy@gmail.com>
2025-03-06 14:13:44 -0600

    added Books and Home

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import Footer from './components/Footer';
 import Highlights from './components/Highlights';
 import Landing from './components/Landing';
 import Nav from './components/Nav';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Books from './components/pages/Books';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Route path="/" exact component={Home}/>
+      <Route path="/books" component={Books} />
       <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Landing from '../Landing'
+import Highlights from '../Highlights'
+import Featured from '../Featured'
+import Discounted from '../Discounted'
+import Explore from '../Explore'
+
+const Home = () => {
+  return (
+    <>
+    <Landing />
+    <Highlights />
+    <Featured />
+    <Discounted />
+    <Explore />
+    </>
+    )
+}
+
+export default Home
